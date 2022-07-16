3a3d75563b646fcea14fad9435e75c1c90322770
Navindu Dananga <navindudananga123@gmail.com>
2022-07-16 16:53:38 +0530

    query to get

--- src/search.rs
+++ src/search.rs
@@ -2,8 +2,7 @@ use crate::gecko;
 use crate::types::Response;
 use serde_json;
 
-pub fn query(query: &str) -> Response<serde_json::Value> {
+pub fn get(query: &str) -> Response<serde_json::Value> {
     let response = gecko::get_request("/search?query=", query);
     response
 }
-

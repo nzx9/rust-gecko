b4f93ec828746aa414da9489b38f0eabf87fe57f
Navindu Dananga <navindudananga123@gmail.com>
2022-07-16 16:50:44 +0530

    categories endpoints created

---
+++ src/categories.rs
@@ -0,0 +1,39 @@
+use crate::gecko;
+use crate::types::Response;
+use serde_json;
+
+pub enum Order {
+    MarketCapDesc,
+    MarketCapAsc,
+    NameDesc,
+    NameAsc,
+    MarketCapChange24hDesc,
+    MarketCapChange24hAsc,
+}
+
+impl Order {
+    fn as_str(&self) -> &'static str {
+        match self {
+            Order::MarketCapDesc => "market_cap_desc",
+            Order::MarketCapAsc => "market_cap_asc",
+            Order::NameDesc => "name_desc",
+            Order::NameAsc => "name_asc",
+            Order::MarketCapChange24hDesc => "market_cap_change_24h_desc",
+            Order::MarketCapChange24hAsc => "market_cap_change_24h_asc",
+        }
+    }
+}
+
+pub fn categories(order: Option<Order>) -> Response<serde_json::Value> {
+    let mut params = "?".to_string();
+    if !order.is_none() {
+        params.push_str(&["order", order.unwrap().as_str()].join("="));
+    }
+    let response = gecko::get_request("/coins/categories", &params);
+    response
+}
+
+pub fn categories_list() -> Response<serde_json::Value> {
+    let response = gecko::get_request("/coins/categories/list", "");
+    response
+}

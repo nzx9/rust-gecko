3ac0c5ab8e466e1b5b2ae8aa2971e08cf9e25803
Navindu Dananga <navindudananga123@gmail.com>
2022-07-16 16:55:12 +0530

    data to get

--- src/global.rs
+++ src/global.rs
@@ -2,7 +2,7 @@ use crate::gecko;
 use crate::types::Response;
 use serde_json;
 
-pub fn data() -> Response<serde_json::Value> {
+pub fn get() -> Response<serde_json::Value> {
     let response = gecko::get_request("/global", "");
     response
 }
